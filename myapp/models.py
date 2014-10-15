b99308f585d299a3993b7dc088feb056b292fa32
giacomo <giacomo.lombardi@live.it>
2014-10-15 19:25:37 +0200

    create project

---
+++ myapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Accelerometer(models.Model):
+
+    MAX_VALUE_OF_SENSOR = 32767
+
+    max_g = models.FloatField(default=2)
+    value_x = models.FloatField(default=0)
+    value_y = models.FloatField(default=0)
+    value_z = models.FloatField(default=-1)
