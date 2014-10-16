dbe221784772a9dea09a044bb40880aef677d19d
giacomo <giacomo.lombardi@live.it>
2014-10-16 14:25:42 +0200

    JSON request --> fail

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+import json
+
+from django.http import HttpResponse
 from django.views import generic
+
+from myapp.models import Accelerometer
 
 
 class AccelerometerView(generic.base.TemplateView):
@@ -11,6 +15,12 @@ class ConnectionView(generic.base.TemplateView):
 
     template_name = "myapp/connection.html"
 
+
 class HomeView(generic.base.TemplateView):
 
     template_name = "myapp/home.html"
+
+
+def accelerometer_json(request):
+    data = {"x":"zero"}
+    return HttpResponse(data, content_type='application/json')
